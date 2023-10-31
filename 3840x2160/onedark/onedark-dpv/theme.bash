8e82007d9c5a7ff1118d3d3cac3a3e96627fcbd4
dpv927 <dpv927@inlumine.ual.es>
2023-10-31 12:29:13 +0100

    Create onedark theme

---
+++ 3840x2160/onedark/onedark-dpv/theme.bash
@@ -0,0 +1,69 @@
+# Theme colors
+background='#101318'
+foreground='#ABB2BF'
+color0='#32363D'
+color1='#E06B74'
+color2='#98C379'
+color3='#E5C07A'
+color4='#62AEEF'
+color5='#C778DD'
+color6='#55B6C2'
+color7='#ABB2BF'
+color8='#50545B'
+color9='#EA757E'
+color10='#A2CD83'
+color11='#EFCA84'
+color12='#6CB8F9'
+color13='#D282E7'
+color14='#5FC0CC'
+color15='#B5BCC9'
+accent='#5294E2'
+
+# Wallpaper
+wdir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
+wallpaper="/home/tux/Pictures/wallpapers/sea.jpg"
+
+# Polybar
+polybar_font='Iosevka Nerd Font:size=20;3'
+
+# Rofi
+rofi_font='Iosevka 18'
+rofi_icon='Arc-Circle'
+
+# Terminal
+terminal_font_name='Cascadia code'
+terminal_font_size='18'
+
+# Appearance
+gtk_font='Noto Sans 9'
+gtk_theme='Arc-Dark'
+icon_theme='Zafiro'
+cursor_theme='Qogirr-Dark'
+
+# Dunst
+dunst_width='300'
+dunst_height='80'
+dunst_offset='10x42'
+dunst_origin='top-right'
+dunst_font='Iosevka Custom 9'
+dunst_border='1'
+dunst_separator='1'
+
+# Picom
+picom_backend='glx'
+picom_corner='10'
+picom_shadow_r='40'
+picom_shadow_o='0.30'
+picom_shadow_x='-20'
+picom_shadow_y='-20'
+picom_blur_method='none'
+picom_blur_strength='0'
+
+# Bspwm
+bspwm_fbc="#292d37"
+bspwm_nbc="#292d37"
+bspwm_abc="${color4}"
+bspwm_pfc="${color6}"
+bspwm_border='0'
+bspwm_gap='20'
+bspwm_sratio='0.50'
