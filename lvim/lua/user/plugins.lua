d519bc8dd4c98dfc05847ef5df3cad3fc67d3167
dpv927 <dpv927@inlumine.ual.es>
2023-10-31 12:29:38 +0100

    Move

--- lvim/lua/user/plugins.lua
+++
@@ -1,7 +0,0 @@
--- Define lvim list of plugins.
-lvim.plugins = {
-  { "catppuccin/nvim", name = "catppuccin", priority = 1000 },
-  {'andweeb/presence.nvim'},
-  {'norcalli/nvim-colorizer.lua'},
-  {'barrett-ruth/live-server.nvim'}
-}
